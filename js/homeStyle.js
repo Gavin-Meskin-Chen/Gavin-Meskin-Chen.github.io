8fe5147473fad1587de68a27f81497be5ac0a2ee
Gavin_Chen <gavin_meskin_chen@163.com>
2023-01-08 19:24:10 +0800

    Site updated: 2023-01-08 19:24:09

---
+++ js/homeStyle.js
@@ -0,0 +1,7 @@
+
+function RandomColor(){
+    this.r = Math.floor(Math.random()*255);
+    this.g = Math.floor(Math.random()*255);
+    this.b = Math.floor(Math.random()*255);
+    this.color = 'rgba('+ this.r +','+ this.g +','+ this.b +',0.8)';
+  }
