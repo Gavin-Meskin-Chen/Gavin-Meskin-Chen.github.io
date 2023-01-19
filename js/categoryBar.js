9500faae27cd108d2ea2248cdec84f75c6a343f0
Gavin_Chen <gavin_meskin_chen@163.com>
2023-01-19 22:10:24 +0800

    Site updated: 2023-01-19 22:10:23

--- js/categoryBar.js
+++ js/categoryBar.js
@@ -3,6 +3,9 @@ topCategoriesBarScroll()
 
 //分类条
 function categoriesBarActive(){
+  if (document.querySelector('#category-bar')){
+    $(".category-bar-item").removeClass("select")
+  }
   var urlinfo = window.location.pathname;
   urlinfo = decodeURIComponent(urlinfo)
   console.log(urlinfo);
@@ -27,8 +30,38 @@ function categoriesBarActive(){
       }
     }
   }
-  
 }
+
+// 如果是标签条则启用，同时注释掉分类条
+// tagsBarActive()
+//标签条
+function tagsBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  //console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#tags-bar')){
+      document.getElementById('首页').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/tags\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    //console.log(patbool);
+    // 获取当前的标签
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      //console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowTag = valuegroup[2];
+      if (document.querySelector('#category-bar')){
+        document.getElementById(nowTag).classList.add("select");
+      }
+    }
+  } 
+}
+
 
 //鼠标控制横向滚动
 function topCategoriesBarScroll(){
