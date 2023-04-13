d5727c598186e6fe91284cbd3bb99bab9645b8e7
Gavin_Chen <gavin_meskin_chen@163.com>
2023-04-13 10:41:41 +0800

    Site updated: 2023-04-13 10:41:40

--- js/datasheet.js
+++ js/datasheet.js
@@ -236,24 +236,24 @@ var JaySongsheet = [
     }
 ];
 
-// WIDGET = {
-//     "CONFIG": {
-//         "modules": "0124",
-//         "background": "5",
-//         "tmpColor": "var(--font-color)",
-//         "tmpSize": "16",
-//         "cityColor": "var(--font-color)",
-//         "citySize": "16",
-//         "aqiColor": "var(--font-color)",
-//         "aqiSize": "16",
-//         "weatherIconSize": "24",
-//         "alertIconSize": "18",
-//         "padding": "10px 0px",
-//         "shadow": "0",
-//         "language": "auto",
-//         "fixed": "false",
-//         "vertical": "top",
-//         "horizontal": "left",
-//         "key": "c364314f2dc349ffae8fda8e6c9cd06c"
-//     }
-// }
+WIDGET = {
+    "CONFIG": {
+        "modules": "0124",
+        "background": "5",
+        "tmpColor": "var(--font-color)",
+        "tmpSize": "16",
+        "cityColor": "var(--font-color)",
+        "citySize": "16",
+        "aqiColor": "var(--font-color)",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 0px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "key": "c364314f2dc349ffae8fda8e6c9cd06c"
+    }
+}
