c78ee79f6cbefa4c36e4f3fcb7ebbcae89d3814b
Gavin_Chen <gavin_meskin_chen@163.com>
2023-08-02 13:41:36 +0800

    Site updated: 2023-08-02 13:41:35

--- js/gavin.js
+++ js/gavin.js
@@ -77,7 +77,7 @@ window.onload = function () {
         saveToLocal.get('theme') == 'light' ? document.querySelector("#set-theme-light input").checked = true : document.querySelector("#set-theme-dark input").checked = true;
     }
     // new Vue().$mount('#aside-system')
-    if (set_notice.checked) tools.showNote("欢迎来到参星阁！", "success", 5);
+    // if (set_notice.checked) tools.showNote("欢迎来到参星阁！", "success", 5);
     console.log("\n %cGC音频控制器 v1.3.2 参星阁出品%c https://gavin-chen.top \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
     console.log(`Welcome to:\n%c参星阁:%c https://gavin-chen.top%c\nThis site has been running stably for %c${Math.round(((new Date).getTime() - new Date("2023/01/04 20:53:58").getTime()) / 864e5)} %c days`, "border:1px #888 solid;border-right:0;border-radius:5px 0 0 5px;padding: 5px 10px;color:white;background:#4976f5;margin:10px 0", "border:1px #888 solid;border-left:0;border-radius:0 5px 5px 0;padding: 5px 10px;", "", "color:#4976f5", "")
 }
@@ -196,17 +196,17 @@ function asideNote() {
     if (date == dates) {
         noteCard.style.display = "";
         noteArea.innerHTML = "<p align='center'>今天月末，做好总结</p>";
-        if (set_notice.checked) tools.showNote("今天月末，做好总结", "warning", 5)
+        // if (set_notice.checked) tools.showNote("今天月末，做好总结", "warning", 5)
     } else if (date == 1) {
         noteCard.style.display = "";
         noteArea.innerHTML = "<p align='center'>今天月初，做好规划</p>";
-        if (set_notice.checked) tools.showNote("今天月初，做好规划", "warning", 5)
+        // if (set_notice.checked) tools.showNote("今天月初，做好规划", "warning", 5)
     } else {
         switch (lunarDay) {
             case "十六":
                 noteCard.style.display = "";
                 noteArea.innerHTML = "<p align='center'>今晚月圆</p>";
-                if (set_notice.checked) tools.showNote("今晚月圆", "success", 5)
+                // if (set_notice.checked) tools.showNote("今晚月圆", "success", 5)
                 break;
             default: noteCard.style.display = "none";
         }
