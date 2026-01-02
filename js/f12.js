8548e76c15be6d887decf46ca6dcf8306c17b56b
Gavin_Chen <gavin_meskin_chen@163.com>
2026-01-03 00:27:21 +0800

    Site updated: 2026-01-03 00:27:21

--- js/f12.js
+++
@@ -1,3 +0,0 @@
-document.onkeydown = function (e) {
-    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return tools.showNote("开发者模式已打开，请遵循GPL协议","warning",5)
-};
--- js/transColor.js
+++
@@ -1,7 +0,0 @@
-
-Shixu: (Q3, Q2, Q1, Q0) => {
-    var Q_3 = Q2
-    var Q_2 = Q1
-    var Q_1 = Q2
-    var Q_0 = Q2
-}
